2c6c121a42abdf9535c304c5d5ce616bfffc3d56
rhayen <rhayen21@gmail.com>
2018-10-09 20:06:45 +0700

    baru

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	 <link href="style.css" rel="stylesheet" />
+	 <meta charset=”UTF-8”>
+	 <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+
+</head>
+<body>
+	<header>
+			
+		<div class="navigasi">
+			<ul>
+			<a href="index.php">Home</a>
+			<a href="profil.php">Profil</a>
+			<a href="maskapai.php">Maskapai</a>
+			<a href="layanan.php">Layanan</a>
+			<a href="galery.php">Galery</a>	
+		</div>
+		</ul>						
+		</div>
+	</header>
+		<div class="kolom" >
+			<div class="kirihome">
+				<div class="dalam-kiri">
+				<?php
+					echo "<center><br><br><h1>Selamat Datang Di Website Revani Travel</h1></br></br></center>";
+				?>
+				</div>
+			</div>
+		
+		</div>
+
+</body>
+
+
+</html>
